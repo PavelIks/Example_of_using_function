7df092ae2c682157ec53a1e617799943920c45a3
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-11-02 16:56:13 +0200

    Updated-1.

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    $array1 =
+        [
+            "Eto prikolnyi prikol",
+            "A vod eda prigol memnyi"
+        ];
+        echo ('<div>'.strtoupper($array1[0]).'</div>');
+        echo ('<div>'.strtoupper($array1[1]).'</div>');
+?>
